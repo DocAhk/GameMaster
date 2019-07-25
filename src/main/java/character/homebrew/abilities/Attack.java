17e86bcaf3c21ec029747770b5010d607f8ae09a
AHouwari <35615967+AHouwari@users.noreply.github.com>
2019-07-25 16:12:49 -0400

    Added a HomeBrew Actor and a generic Attack ability

---
+++ src/main/java/character/homebrew/abilities/Attack.java
@@ -0,0 +1,12 @@
+package character.homebrew.abilities;
+
+import character.Ability;
+
+public class Attack implements Ability {
+    int initiatorStrength;
+    int opposerAgility;
+
+    public void useAbility() {
+
+    }
+}
