45d55e53d4bf8358106470e7fc647350599ce9a7
AHouwari <35615967+AHouwari@users.noreply.github.com>
2019-07-25 16:12:32 -0400

    Added a HomeBrew Actor and a generic Attack ability

---
+++ src/main/java/character/homebrew/HomeBrewActor.java
@@ -0,0 +1,18 @@
+package character.homebrew;
+
+import character.Ability;
+import character.Actor;
+import character.Stat;
+
+import java.util.List;
+
+//HomeBrew Actor
+public class HomeBrewActor extends character.Actor {
+    private List<Ability> abilities;
+
+    private List<Stat> attributes;
+
+    private List<Stat> skills;
+
+
+}
