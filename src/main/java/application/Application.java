ab8e2b5126230559c724a911235573208e249363
AHouwari <35615967+AHouwari@users.noreply.github.com>
2019-07-26 13:50:04 -0400

    Created basic web service

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,9 +1,12 @@
 package application;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,18 +1,19 @@
 package application;
 
 import logic.Dice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class Controller {
 
     private Dice die = new Dice(20);
 
-    public int diceRoll(int sides) {
+    @GetMapping("diceRoll")
+    public int diceRoll(@RequestParam(value="sides", defaultValue="20") int sides) {
         die.setSides(sides);
         return die.roll();
     }
 
-    public int diceRoll() {
-        return die.roll();
-    }
 }
